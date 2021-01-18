5b0de1387ef3ec24355a37ca5c2215ae4a888b43
justinkelley18 <77300319+justinkelley18@users.noreply.github.com>
2021-01-18 17:46:12 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+var Name = "Justin Kelley";
+console.log(Name)
+// America has 50 states.
+const states = '50'
+console.log(states)
+// practing different variable and operators.
+let num1 = 5
+let num2 = 4
+console.log(add(num1,num2));
+function add(num1,num2){
+    return num1 + num2
+}
+// Dog
+let Name1 = "Rosie"
+let breed = "Pitbull"
+var pet = {
+    "Name": Name1,
+    'breed': breed
+}
+console.log(pet);
+// Alerts
+function SayHello(params) {
+    alert('Hello World!')  
+}
+SayHello()
+
+let name2 = "Charles" 
+let age1 = 21 ; 
+var Charles = {
+    "Name": name2 ,
+    "Age": age1
+}
+
+let name3 ='Abby'
+let age2 = 27 ;
+var Abby = {
+    "Name":name3,
+    "Age":age2
+}
+
+let name4 = 'James'
+let age3 = 18 ;
+var James = {
+    "Name": name4,
+    "Age": age3
+}
+
+let name5 = 'John'
+let age4 = 17 ;
+var John = {
+    "Name":name5,
+    "Age": age4
+}
+
+console.log(Charles); 
+
+var vegetables = [Broccoli],[Asparagus],[Zuchinni],[Brussels],[Corn];
+var people = [Name],[name2],[name3],[name4],[name5];
+
+function checkAge(name2, age1) {
+    return age>21
+    alert("Sorry " + name2 + ", you aren't old enough to view this page!");
+}
+ checkAge()
+
+ }
+
+   
+
+
+
+
